833615cd216b17fc0ef3cac2163148754c39aa5d
Optinno <anand.kaswa08@gmail.com>
2025-06-09 15:29:19 +0530

    second commit

---
+++ AddLibraryOfAddition/src/main/java/com/addition/addlibraryofaddition/AdditionOf2Numbers.java
@@ -0,0 +1,10 @@
+package com.addition.addlibraryofaddition;
+
+public class AdditionOf2Numbers {
+
+    public Double additionHappens(double amount1, double amount2){
+
+        return (amount1+amount2);
+
+    }
+}
