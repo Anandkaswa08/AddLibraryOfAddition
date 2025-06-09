4495f3d7fd52ae445e9c024de86ba4803b70d21e
Anand <anand.kaswa08@gmail.com>
2025-06-09 15:58:57 +0530

    Fourth

--- AddLibraryOfAddition/src/main/java/com/addition/addlibraryofaddition/AdditionOf2Numbers.java
+++ AddLibraryOfAddition/src/main/java/com/addition/addlibraryofaddition/AdditionOf2Numbers.java
@@ -2,17 +2,9 @@ package com.addition.addlibraryofaddition;
 
 public class AdditionOf2Numbers {
 
-    public Double additionHappens(double amount1, double amount2){
-
-
+    public Double doAddition(double amount1, double amount2){
         //AAAAbb
-
-
-
-
-
-
-        return (amount1+amount2);
+      return (amount1+amount2);
 
     }
 }
